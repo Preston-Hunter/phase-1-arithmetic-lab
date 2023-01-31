b3fb35796ceca65a9aecf9caa9b193b26d1e5cea
PrestonHunter <prestonhunter2018@gmail.com>
2023-01-31 10:11:14 -0700

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 =31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+
+let random = Math.floor(((Math.random() - 1)  * -1) * 1000);
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+let set = (1,2,3,20);
+let max= Math.max(set);
+
+
